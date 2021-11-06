6673fd61227433814819c68b759629ed6c5381a8
lea.arturi <lea.arturi@gmail.com>
2021-11-06 20:18:08 -0300

    Overwrite User Create with make_password

--- icard/users/api/views.py
+++ icard/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from .serializers import UserSerializer
@@ -8,3 +9,7 @@ class UserApiViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
