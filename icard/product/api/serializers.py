cb44ca012fffade22c941934c2bc8d44bcfae134
lea.arturi <lea.arturi@gmail.com>
2021-11-14 14:48:28 -0300

    Muestra el detalle de la categoria en ProductSerializer

--- icard/product/api/serializers.py
+++ icard/product/api/serializers.py
@@ -2,9 +2,22 @@ from django.db import models
 from rest_framework.serializers import ModelSerializer
 
 from product.models import Product
+from category.api.serializers import CategorySerializer
 
 class ProductSerializer(ModelSerializer):
 
+    category_data = CategorySerializer(
+        source='category',
+        read_only=True
+    )
     class Meta:
         model = Product
-        fields = ['id', 'title', 'image', 'price', 'active', 'category']
+        fields = [
+            'id', 
+            'title', 
+            'image', 
+            'price', 
+            'active', 
+            'category', 
+            'category_data'
+        ]
