d2b7caaf403c26410f35e0a72e647a8afc1bc30e
lea.arturi <lea.arturi@gmail.com>
2021-11-28 12:27:01 -0300

    CRUD Payment - Ordering and Filters

--- icard/payment/api/views.py
+++ icard/payment/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from payment.models import Payment
 from payment.api.serializers import PaymentSerializer
@@ -6,3 +8,6 @@ from payment.api.serializers import PaymentSerializer
 class PaymentApiView(ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['table', 'status_payment', 'payment_type']
+    ordering_fields = '__all__'
