c23161ec68ba401fae328511d346b458f78ba9bd
lea.arturi <lea.arturi@gmail.com>
2021-11-29 14:45:30 -0300

    Add table_data on PaymentSerializer

--- icard/payment/api/serializers.py
+++ icard/payment/api/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
 from payment.models import Payment
+from table.api.serializers import TableSerializer
 
 class PaymentSerializer(ModelSerializer):
+
+    table_data = TableSerializer(source="table", read_only=True)
     class Meta:
         model = Payment
         fields = (
             'id',
             'table',
+            'table_data',
             'total_payment',
             'payment_type',
             'status_payment',
